0673ba01708c309520c21125f537c43337f29fa2
c22quiambao <christelquiambao@gmail.com>
2024-01-31 15:23:11 -0800

    solution to exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Jughead", last_name: "Jones", hourly_rate: 40)
+@store1.employees.create(first_name: "Betty", last_name: "Cooper", hourly_rate: 30)
+@store1.employees.create(first_name: "Veronica", last_name: "Lodge", hourly_rate: 50)
+@store2.employees.create(first_name: "Archie", last_name: "Andrews", hourly_rate: 75)
+@store2.employees.create(first_name: "Christel", last_name: "Vegamora", hourly_rate: 80)
+@store2.employees.create(first_name: "James", last_name: "Quiambao", hourly_rate: 45)
